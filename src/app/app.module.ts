02157c357c15e034b003810e7b9cf756010bfc2b
phenphak <phenphak.tae86@gmail.com>
2021-06-15 16:34:41 +0700

    lab31

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {HttpClientModule} from '@angular/common/http';
+import {FormsModule,ReactiveFormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { PostListComponent } from './post-list/post-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/post-list/post-list.component.ts
@@ -0,0 +1,42 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-post-list',
+  templateUrl: './post-list.component.html',
+  styleUrls: ['./post-list.component.css'],
+})
+export class PostListComponent implements OnInit {
+  posts: any[];
+  form: FormGroup;
+  constructor(private httpClient: HttpClient, private fb: FormBuilder) {
+    this.form = this.fb.group({
+      userId: '',
+      title: '',
+      body: ''
+    });
+  }
+
+  ngOnInit(): void {
+    this.loadPost();
+  }
+  loadPost() {
+    this.posts = [];
+    this.httpClient
+      .get('http://jsonplaceholder.typicode.com/posts')
+      .subscribe((result) => {
+        this.posts = result as any[];
+      });
+  }
+
+  addPost(){
+    const newPost = this.form.value;
+    this.httpClient.post('http://jsonplaceholder.typicode.com/posts',newPost)
+      .subscribe(result =>{
+      this.form.reset();
+      alert('Add Post Success !')
+      this.loadPost();
+    })
+  }
+}
